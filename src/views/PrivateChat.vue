26b69cd1967db409ba905581e94733a124361fb8
Khoa Hoang <whoskhoahoang@gmail.com>
2020-04-05 18:41:50 +0900

    Can view chat messages in real-time

--- src/views/PrivateChat.vue
+++ src/views/PrivateChat.vue
@@ -93,42 +93,12 @@
             </div>
             <div class="mesgs">
               <div class="msg_history">
-                <div class="incoming_msg">
+                <div v-for="message in messages" class="incoming_msg">
                   <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
                   <div class="received_msg">
                     <div class="received_withd_msg">
-                      <p>Test which is a new approach to have all
-                        solutions</p>
+                      <p>{{ message.message }}</p>
                       <span class="time_date"> 11:01 AM    |    June 9</span></div>
-                  </div>
-                </div>
-                <div class="outgoing_msg">
-                  <div class="sent_msg">
-                    <p>Test which is a new approach to have all
-                      solutions</p>
-                    <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-                </div>
-                <div class="incoming_msg">
-                  <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                  <div class="received_msg">
-                    <div class="received_withd_msg">
-                      <p>Test, which is a new approach to have</p>
-                      <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
-                  </div>
-                </div>
-                <div class="outgoing_msg">
-                  <div class="sent_msg">
-                    <p>Apollo University, Delhi, India Test</p>
-                    <span class="time_date"> 11:01 AM    |    Today</span> </div>
-                </div>
-                <div class="incoming_msg">
-                  <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                  <div class="received_msg">
-                    <div class="received_withd_msg">
-                      <p>We work directly with our designers and suppliers,
-                        and sell direct to you, which means quality, exclusive
-                        products, at a price anyone can afford.</p>
-                      <span class="time_date"> 11:01 AM    |    Today</span></div>
                   </div>
                 </div>
               </div>
@@ -149,9 +119,12 @@
 <script>
 
 export default {
+  name: "PrivateChat",
+
   data() {
     return {
-      message: null  // Specified by v-mode1
+      message: null,  // Specified by v-mode1
+      messages: []
     }
   },
 
@@ -159,13 +132,36 @@ export default {
     saveMessage() {
       // Save to Firestore
       db.collection("chat").add({
-        message: this.message
+        message: this.message,
+        createdAt: new Date(),
+
       }).then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
       }).catch(function(error) {
         console.log("Error adding document: ", error);
       });
+
+      // Clear input field
+      this.message = null;
+    },
+
+    fetchMessages() {
+      // onSnapshot() makes real-time display of messages possible. This
+      // method call seems to contain the call to get(), which gets the
+      // message data from our "chat" DB.
+      db.collection("chat").orderBy("createdAt").onSnapshot((querySnapshot) => {
+        let allMessages = [];
+        querySnapshot.forEach((doc) => {
+          allMessages.push(doc.data());
+        });
+
+        this.messages = allMessages;
+      });
     }
+  },
+
+  created() {
+    this.fetchMessages();
   }
 }
 </script>
